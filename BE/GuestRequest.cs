b340a9f8c13400c0a3b40ab27f2d070e6883b320
rgrin4365@gmail.com <rgrin4365@gmail.com>
2019-12-16 17:16:21 +0200

    GueatRequest

--- BE/GuestRequest.cs
+++ BE/GuestRequest.cs
@@ -8,5 +8,34 @@ namespace BE
 {
     public class GuestRequest
     {
+        public int guestRequestKey { get; }
+        string name;
+        string LastName;
+        string Mail;
+        public Enums.OrderStatus Status { get; set; }
+        DateTime Registration;
+        DateTime entryDate;
+        DateTime releaseDate;
+        string area;
+        string subArea;
+        Enums.HostingUnitType TypeOfUnit;
+        int numAdult;
+        int numChildren;
+        Enums.Preference pool;
+        Enums.Preference jacuzzi;
+        Enums.Preference garden;
+        Enums.Preference childrenAttractions;
+        //tostring
+        public GuestRequest(int fName, int lName, string mail, DateTime enter, DateTime rel, string area, string sArea, Enums.HostingUnitType type, int nAdult, int Nchild, Enums.Preference isPool, Enums.Preference isJacuzzi, Enums.Preference isGarden, Enums.Preference.isAttractions)
+        {
+            guestRequestKey = Configuration.GuestRequest;
+
+        }
+        public override string ToString()
+        {
+            return "First Name: " + name + "Last Name: " + LastName + "Mail: " + Mail + "Order status: " + Status + "Date of registration: " + Registration + "Entry day: " + entryDate + "Release date: " + releaseDate + "Area: " + area + "Sub area: " + subArea + "Type of unit: " + TypeOfUnit +
+                "Number of adults: " + numAdult + "Number of children: " + numChildren + "Want pool: " + pool + "Want jecuzzi: " + jacuzzi + "Want garden: " + garden + "want children attractions" + childrenAttractions;
+        }
+        
     }
 }
