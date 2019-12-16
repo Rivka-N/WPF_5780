d554ebdff951a4e899539697bab382bfc0506e8d
רבקה נ <rnadell@g.jct.ac.il>
2019-12-16 15:48:58 +0200

    added hosts

--- BE/Host.cs
+++ BE/Host.cs
@@ -8,6 +8,19 @@ namespace BE
 {
     public class Host
     {
+        public int HostKey { get; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Mail { get; set; }
+        public int PhoneNumber { get; set; }
+        public BankAccount Bank { get; set; }
+        public bool CollectionClearance { get; set; }
         public Host() { }
+        public override string ToString()
+        {
+            return "Owner id: " + HostKey + "Name: " + Name + " " + LastName + "Phone number: " + PhoneNumber +
+                "Mail address: " + Mail + "Bank Account: " + Bank + "Collection permission: " + CollectionClearance;
+        }
+
     }
 }
