d117f80a200dbecfda706224ae5b2fa4ba04d626
Rivka-N <rivka.hadara@gmail.com>
2019-12-17 16:31:41 +0200

    added to wpf guest

--- PL/guestOptions.xaml.cs
+++ PL/guestOptions.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using BE;
 
 namespace PL
 {
@@ -22,8 +23,21 @@ namespace PL
         public guestOptions()
         {
             InitializeComponent();
+            GuestRequest g1 = new GuestRequest();
+            
         }
 
-     
+        private void cl_addStartDate(object sender, SelectionChangedEventArgs e)
+        {
+            #region endDate
+            //cl_endDate starts from the day after start date
+            //updates the statdate in guest
+            //add field to guest to update date only if it's not set yet.
+            //cl_EndDate.DisplayDateStart = tomorrow.AddDays(1);//sets the start date to tomorrow so the stay will be at least one day long
+            cl_EndDate.Visibility = Visibility.Visible;//makes enddate visible to choose from now
+            tb_EndDate.Visibility = Visibility.Visible;
+            #endregion
+            
+        }
     }
 }
