4e836bcb42fb630ab599b574e447af5f37ac1ffe
rgrin4365@gmail.com <rgrin4365@gmail.com>
2019-12-21 21:01:58 +0200

    del

--- consolePL/Program.cs
+++ consolePL/Program.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BE;
+
 
 namespace consolePL
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            
-        }
+     
+   
     }
 }
